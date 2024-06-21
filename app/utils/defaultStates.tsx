45f646c1744706e28c9949676eff96bbf12aec2e
bbwebdevelop <bbwebdevelop@gmail.com>
2024-06-21 08:59:51 +0200

    modern-weather-api-app
    react.js/typescript/api

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Warszawa",
+    country: "PL",
+    state: "Mazowieckie",
+    lat: 52.2297,
+    lon: 21.0122,
+  },
+  {
+    name: "Kraków",
+    country: "PL",
+    state: "Małopolskie",
+    lat: 50.0647,
+    lon: 19.945,
+  },
+  {
+    name: "Łódź",
+    country: "PL",
+    state: "Łódzkie",
+    lat: 51.7592,
+    lon: 19.455,
+  },
+  {
+    name: "Wrocław",
+    country: "PL",
+    state: "Dolnośląskie",
+    lat: 51.1079,
+    lon: 17.0385,
+  },
+  {
+    name: "Poznań",
+    country: "PL",
+    state: "Wielkopolskie",
+    lat: 52.4064,
+    lon: 16.9252,
+  },
+];
+
+export default defaultStates;
